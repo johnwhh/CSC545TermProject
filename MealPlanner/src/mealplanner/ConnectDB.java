f30f10a27318604677a120279b3888f7dac2e3dc
Matthew <maa92498@gmail.com>
2021-04-18 15:32:27 -0400

    Add database connection class

---
+++ MealPlanner/src/mealplanner/ConnectDB.java
@@ -0,0 +1,94 @@
+// This project has no license.
+package mealplanner;
+
+/**
+ * @date 18-04-2021
+ * @author Matthew
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.jdbc.OracleResultSet;
+
+public class ConnectDB {
+    public static Connection setupConnection()
+    {
+        /*
+         Specify the database you would like to connect with:
+         - protocol
+         - vendor
+         - driver
+         - server ip
+         - port number
+         - database instance name
+         */
+        
+        String jdbcDriver = Secrets.JDBCDRIVER;
+        String jdbcUrl = Secrets.JDBCURL;  // URL for the database
+
+        /*
+         Specify the user account you will use to connect with the database:
+         - user name (e.g., myName)
+         - password (e.g., myPassword)
+         */
+        
+        String username = Secrets.USERNAME;
+        String password = Secrets.PASSWORD;
+        
+        try
+        {
+            // Load jdbc driver.            
+            Class.forName(jdbcDriver);
+            
+            // Connect to the Oracle database
+            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            return conn;
+        }
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    
+    static void close(Connection conn) 
+    {
+        if(conn != null) 
+        {
+            try
+            {
+                conn.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+
+    static void close(OraclePreparedStatement st)
+    {
+        if(st != null)
+        {
+            try
+            {
+                st.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+
+    static void close(OracleResultSet rs)
+    {
+        if(rs != null)
+        {
+            try
+            {
+                rs.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+}
